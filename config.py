6cec21a21bc737fb019414eb25efd1c0560389e4
akshattrivedi <akshat.trivedi08@gmail.com>
2021-05-23 01:12:14 +0530

    feat: Singleton Configurations Class and Config JSON

---
+++ config.py
@@ -0,0 +1,18 @@
+import json
+
+class Config:
+    __instance = None
+
+    @staticmethod
+    def getInstance():
+        if Config.__instance == None:
+            Config()
+        
+        return Config.__instance
+
+    def __init__(self):
+        if Config.__instance != None:
+            raise Exception ("This is a Singleton Configuration Class!")
+        else:
+            Config.__instance = self
+            self.data = json.load(open("config.json"))
