8443ae6539e77322fc316fed09dd8633e3a0f384
akshattrivedi <akshat.trivedi08@gmail.com>
2021-05-27 22:26:43 +0530

    feat: Tabular Representation for Find Slots Button and Code Modularity

--- templates/static/js/global.js
+++ templates/static/js/global.js
@@ -83,6 +83,30 @@ $(function () {
     });
 });
 
+
+
+// API Call for Finding Slots Through Find Slots Button
+$(document).ready(function () {
+    var centersString = $('#hidden-slots').val();
+
+    if (centersString == ""){
+        document.getElementById('slots-label').innerHTML = "";
+        return;
+    }
+
+    
+    if (centersString == "[]"){
+        document.getElementById('slots-label').innerHTML = "NO SLOTS AVAILABLE!";
+        return;
+    }
+
+    var centersStringJSON = centersString.replace(/'/g, '"');
+    var centers = JSON.parse(centersStringJSON);
+
+    createTabularCenters(centers);
+    
+});
+
 // API Call for Finding Available Slots Through Auto-Refresh of UI Component
 $(function () {
     $('#auto-refresh').click(function () {
@@ -104,63 +128,7 @@ $(function () {
                 },
                     function (data) {
                         var centers = data["slots"];
-
-                        if (centers.length > 0) {
-                            var slotsTable = "<table>";
-
-                            slotsTable += "<tr>";
-                            slotsTable += "<th>" + "NAME" + "</th>";
-                            slotsTable += "<th>" + "ADDRESS" + "</th>";
-                            slotsTable += "<th>" + "STATE" + "</th>";
-                            slotsTable += "<th>" + "DISTRICT" + "</th>";
-                            slotsTable += "<th>" + "BLOCK" + "</th>";
-                            slotsTable += "<th>" + "PINCODE" + "</th>";
-                            slotsTable += "<th>" + "FEE" + "</th>";
-                            slotsTable += "<th>" + "DATE" + "</th>";
-                            slotsTable += "<th>" + "TOTAL AVAILABILITY" + "</th>";
-                            slotsTable += "<th>" + "AGE" + "</th>";
-                            slotsTable += "<th>" + "VACCINE" + "</th>";
-                            slotsTable += "<th>" + "DOSE 1 AVAILABILITY" + "</th>";
-                            slotsTable += "<th>" + "DOSE 2 AVAILABILITY" + "</th>";
-                            slotsTable += "</tr>";
-
-                            for (var i = 0; i < centers.length; i++) {
-                                var center = centers[i];
-                                var sessions = center["sessions"];
-
-                                slotsTable += '<tr>'
-                                slotsTable += '<td rowspan="' + sessions.length + '">' + center["name"] + '</td>';
-                                slotsTable += '<td rowspan="' + sessions.length + '">' + center["address"] + '</td>';
-                                slotsTable += '<td rowspan="' + sessions.length + '">' + center["state_name"] + '</td>';
-                                slotsTable += '<td rowspan="' + sessions.length + '">' + center["district_name"] + '</td>';
-                                slotsTable += '<td rowspan="' + sessions.length + '">' + center["block_name"] + '</td>';
-                                slotsTable += '<td rowspan="' + sessions.length + '">' + center["pincode"] + '</td>';
-                                slotsTable += '<td rowspan="' + sessions.length + '">' + center["fee_type"] + '</td>';
-
-                                for (var j = 0; j < sessions.length; j++) {
-                                    var session = sessions[j];
-
-                                    if (j!=0){
-                                        slotsTable += '<tr>';
-                                    }
-                                    slotsTable += '<td>' + session['date'] + '</td>';
-                                    slotsTable += '<td>' + session['available_capacity'] + '</td>';
-                                    slotsTable += '<td>' + session['min_age_limit'] + '</td>';
-                                    slotsTable += '<td>' + session['vaccine'] + '</td>';
-                                    slotsTable += '<td>' + session['available_capacity_dose1'] + '</td>';
-                                    slotsTable += '<td>' + session['available_capacity_dose2'] + '</td>';
-                                    slotsTable += '</tr>';
-                                }
-
-                                // slotsTable += '</tr>';
-                            }
-
-                            slotsTable += "</table>";
-                            document.getElementById('slots-label').innerHTML = slotsTable;
-                        }
-                        else {
-                            document.getElementById('slots-label').innerHTML = "NO SLOTS AVAILABLE!";
-                        }
+                        createTabularCenters(centers);
                     });
                 counter = configJSON.autoRefresh / 1000;
                 document.getElementById('auto-refresh-label').innerHTML = "Auto Refreshing in " + counter + " seconds";
@@ -168,6 +136,68 @@ $(function () {
         }, 1000);
     });
 });
+
+function createTabularCenters(centers){
+    if (centers.length > 0) {
+        var slotsTable = "<table>";
+
+        slotsTable += "<tr>";
+        slotsTable += "<th>" + "S.NO." + "</th>";
+        slotsTable += "<th>" + "NAME" + "</th>";
+        slotsTable += "<th>" + "ADDRESS" + "</th>";
+        slotsTable += "<th>" + "STATE" + "</th>";
+        slotsTable += "<th>" + "DISTRICT" + "</th>";
+        slotsTable += "<th>" + "BLOCK" + "</th>";
+        slotsTable += "<th>" + "PINCODE" + "</th>";
+        slotsTable += "<th>" + "FEE" + "</th>";
+        slotsTable += "<th>" + "DATE" + "</th>";
+        slotsTable += "<th>" + "TOTAL AVAILABILITY" + "</th>";
+        slotsTable += "<th>" + "AGE" + "</th>";
+        slotsTable += "<th>" + "VACCINE" + "</th>";
+        slotsTable += "<th>" + "DOSE 1 AVAILABILITY" + "</th>";
+        slotsTable += "<th>" + "DOSE 2 AVAILABILITY" + "</th>";
+        slotsTable += "</tr>";
+
+        for (var i = 0; i < centers.length; i++) {
+            var center = centers[i];
+            var sessions = center["sessions"];
+
+            slotsTable += '<tr>';
+            slotsTable += '<td rowspan="' + sessions.length + '">' + (i+1) + '</td>';
+            slotsTable += '<td rowspan="' + sessions.length + '">' + center["name"] + '</td>';
+            slotsTable += '<td rowspan="' + sessions.length + '">' + center["address"] + '</td>';
+            slotsTable += '<td rowspan="' + sessions.length + '">' + center["state_name"] + '</td>';
+            slotsTable += '<td rowspan="' + sessions.length + '">' + center["district_name"] + '</td>';
+            slotsTable += '<td rowspan="' + sessions.length + '">' + center["block_name"] + '</td>';
+            slotsTable += '<td rowspan="' + sessions.length + '">' + center["pincode"] + '</td>';
+            slotsTable += '<td rowspan="' + sessions.length + '">' + center["fee_type"] + '</td>';
+
+            for (var j = 0; j < sessions.length; j++) {
+                var session = sessions[j];
+
+                if (j!=0){
+                    slotsTable += '<tr>';
+                }
+                slotsTable += '<td>' + session['date'] + '</td>';
+                slotsTable += '<td>' + session['available_capacity'] + '</td>';
+                slotsTable += '<td>' + session['min_age_limit'] + '</td>';
+                slotsTable += '<td>' + session['vaccine'] + '</td>';
+                slotsTable += '<td>' + session['available_capacity_dose1'] + '</td>';
+                slotsTable += '<td>' + session['available_capacity_dose2'] + '</td>';
+                slotsTable += '</tr>';
+            }
+
+            // slotsTable += '</tr>';
+        }
+
+        slotsTable += "</table>";
+        document.getElementById('slots-label').innerHTML = slotsTable;
+    }
+    else {
+        document.getElementById('slots-label').innerHTML = "NO SLOTS AVAILABLE!";
+    }
+}
+
 
 // Show/Hide Form Fields Based on Check Boxes
 $(document).ready(function () {
