cecc934a23d3ce27ad0112f5b15ed877fd24789b
akshattrivedi <akshat.trivedi08@gmail.com>
2021-05-29 17:25:13 +0530

    heroku: settings reverted

--- myvaccinenotifier/settings.py
+++ myvaccinenotifier/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
-import django_heroku
+# import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -135,4 +135,4 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
